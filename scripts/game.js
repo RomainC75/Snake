0c989ceb1f84c8ddc8a657a73abaa1dd3c2b9655
RomainC75 <rom.chenard@gmail.com>
2022-05-29 14:43:21 +0200

    no matrix

--- scripts/game.js
+++ scripts/game.js
@@ -31,33 +31,36 @@ class Game {
         do{
             newPrey=[Math.floor(Math.random()*20),Math.floor(Math.random()*20)]
         }while( !this.snake.positions.some(position=>position[0]===newPrey[0] && position[0]===newPrey[0] ))
-        prey=newPrey
+        this.prey=newPrey
     }
-    getCurrentBoard(){
-        const toReturn = this.matrix.map((el,y,fullArray)=>{
-            return el.map((unit, x, line)=>{
-                return this.snake.positions.some( coord => coord[0]===y && coord[1]===x) ? 1 : 0
-            })
-        })
-        return toReturn
-    }
+    // getCurrentBoard(){
+    //     const toReturn = this.matrix.map((el,y,fullArray)=>{
+    //         return el.map((unit, x, line)=>{
+    //             return this.snake.positions.some( coord => coord[0]===y && coord[1]===x) ? 1 : 0
+    //         })
+    //     })
+    //     return toReturn
+    // }
     nextStep(){
+        this.snake.currentDirection=this.snake.nextDirection
         this.snake.moveLengthen()
     }
+
     isGameLost(){
         console.log(this.snake.positions[0])
         return (this.snake.positions[0][0]===-1 || this.snake.positions[0][0]===20) || (this.snake.positions[0][1]===-1 || this.snake.positions[0][1]===20) ? true : false
     }
     changeSnakeDirection(key){
         console.log(key)
+        this.snake.nextDirection=this.snake.currentDirection
         if(key==='ArrowLeft'){
-            this.snake.direction = (this.snake.direction-1)%4
-            if(this.snake.direction===-1){
-                this.snake.direction=3
+            this.snake.nextDirection = (this.snake.nextDirection-1)%4
+            if(this.snake.nextDirection===-1){
+                this.snake.nextDirection=3
             }
         }else if(key==='ArrowRight'){
-            this.snake.direction = (this.snake.direction+1)%4
+            this.snake.nextDirection = (this.snake.nextDirection+1)%4
         }
-        console.log('---->',this.snake.direction)
+        console.log('---->',this.snake.nextDirection)
     }
 }
--- scripts/script.js
+++ scripts/script.js
@@ -6,28 +6,39 @@ for(let i=0 ; i<400 ; i++){
 }
 
 const game = new Game()
+game.getNewPrey()
 let intervalId = false
 
 window.addEventListener('keydown',(e)=>{
     game.changeSnakeDirection(e.key)
 })
 
-
-
 const display = () =>{
-    game.getCurrentBoard().flat().forEach( (unit, i) =>{
+    // game.getCurrentBoard().flat().forEach( (unit, i) =>{
+    //     const div = gameBoard.querySelector(`div:nth-child(${i+1})`)
+    //     div.removeAttribute('class')
+    //     if(unit==1){
+    //         div.classList.add('snake')    
+    //         console.log('add snake')
+    //     }else if(unit==2){
+    //         div.classList.add('prey')
+    //     }
+    // })
+    let line = 0
+    for(let i=0 ; i<400 ; i++){
+        line=Math.floor(i/20)
+        console.log(line)
         const div = gameBoard.querySelector(`div:nth-child(${i+1})`)
         div.removeAttribute('class')
-        if(unit==1){
+        if(game.snake.positions.some(coords=>coords[0]===line&&coords[1]===i-(20*line))){
             div.classList.add('snake')    
             console.log('add snake')
-        }else if(unit==2){
-            div.classList.add('prey')
         }
-    })
+    }
+    const preyCoords=game.prey
+    const preyDive = gameBoard.querySelector(`div:nth-child(${preyCoords[0]*20+preyCoords[1]})`)
+    preyDive.classList.add('prey')
 }
-
-
 
 const start = () =>{
     intervalId = setInterval( ()=>{
